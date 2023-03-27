640996e94da28b60b148d2424bf628da7bb3b642
ikoote1 <ikootepreim@gmail.com>
2023-03-28 01:19:49 +0300

    Adding lists

--- src/components/InputToDo.js
+++ src/components/InputToDo.js
@@ -1,11 +1,15 @@
-function InputTodo() {
+import { useState } from "react";
+
+function InputTodo({onAdd}) {
+  const [textList, setTextList] = useState("");
+  function handleSubmit(ev){
+    ev.preventDefault();
+  }
   return (
-    <>
-      <form>
-        <input type="text" placeholder="todo..........." className="add-text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={textList} onChange={ev => setTextList(ev.target.value)} placeholder="todo..........." className="add-text" />
         <button type="submit" className="submit-button">+</button>
       </form>
-    </>
   );
 }
 
