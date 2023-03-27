341e2f83e9b0274420a42ac2d83a5d96f2df6ea9
ikoote1 <ikootepreim@gmail.com>
2023-03-27 14:12:25 +0300

    Trying checkbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,15 @@
-const TodoItem = ({ itemProp }) => {
+const TodoItem = ({ itemProp, setTodos }) => {
+
+    const handleChange = (id) => {
+        console.log('clicked', id);
+      };
     return (
       <li>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
         {itemProp.title}
       </li>
     );
